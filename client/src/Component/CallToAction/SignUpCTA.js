852ef88a959d0ddaf319abdd5a36a42cae30edb8
Gary Grant <garygrant693@gmail.com>
2022-08-21 00:39:45 -0400

    refactored component files and direcotries

--- client/src/Component/CallToAction/SignUpCTA.js
+++ client/src/Component/CallToAction/SignUpCTA.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// import styles from './SignUpCTA.module.css';
 
 const SignUpCTA = (props) =>{
     return (
         <React.Fragment>
-
+           <div>
+              
+           </div>
         </React.Fragment>
 
     )
--- client/src/Screens/Home.js
+++ client/src/Screens/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Header from "../Component/Header";
-import FeaturedImg from '../Component/FeaturedImg';
-import Introduction from '../Component/Introduction';
+import Header from "../Component/UI/Header";
+import FeaturedImg from '../Component/UI/FeaturedImg';
+import Introduction from '../Component/UI/Introduction';
 
 const Home = (props) =>{
     return (
         <React.Fragment>
            <Header />
+           {/* the conditional render of the SignUpCTA component goes here */}
            <FeaturedImg />
            <Introduction />
         </React.Fragment>
