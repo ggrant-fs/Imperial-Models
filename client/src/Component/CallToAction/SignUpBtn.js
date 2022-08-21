994729ff3bf2380808d5bc054ae02c9d93938b2d
Gary Grant <garygrant693@gmail.com>
2022-08-21 00:45:17 -0400

    created SignUpBtn component

---
+++ client/src/Component/CallToAction/SignUpBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './SignUpCTA.module.css';
+
+const SignUpBtn = (props) => {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+export default SignUpBtn;
