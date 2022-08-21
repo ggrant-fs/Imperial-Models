2a16cc0e719c17f87a1ea9b6ed3861a410df7063
Gary Grant <garygrant693@gmail.com>
2022-08-21 00:50:36 -0400

    created Footer component

---
+++ client/src/Component/UI/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = (props) =>{
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+export default Footer;
