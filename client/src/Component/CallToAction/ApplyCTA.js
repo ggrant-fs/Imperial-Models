dcded44580b8c7c254f813781b14a4380b02b8f8
Gary Grant <garygrant693@gmail.com>
2022-08-23 02:15:00 -0400

    positioned the footer component at the bottom of the Home Screen and made corrections to the styling of introduction.module.css

--- client/src/Component/CallToAction/ApplyCTA.js
+++ client/src/Component/CallToAction/ApplyCTA.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ApplyBtn from './ApplyBtn';
-import styles from './ApplyCTA.module.css';
+// import styles from './ApplyCTA.module.css';
 
 const ApplyCTA= (props) =>{
     return (
--- client/src/Component/UI/Footer.js
+++ client/src/Component/UI/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
-// import styles from './Footer.module.css';
+import styles from './Footer.module.css';
 
 const Footer = (props) =>{
     return (
         <React.Fragment>
-
+          <footer className={styles['main-footer']}>
+             <h2>This is the footer</h2>
+          </footer>
         </React.Fragment>
     )
 }
