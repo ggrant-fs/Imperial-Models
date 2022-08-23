9d20268b049a36009c5882bfbe71ef36cfccb31b
Gary Grant <garygrant693@gmail.com>
2022-08-22 21:01:54 -0400

    added Footer component to Home Screen component

--- client/src/Screens/Home.js
+++ client/src/Screens/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "../Component/UI/Header";
+import Footer from "../Component/UI/Footer";
 import FeaturedImg from '../Component/UI/FeaturedImg';
 import Introduction from '../Component/UI/Introduction';
 
@@ -10,6 +11,7 @@ const Home = (props) =>{
            {/* the conditional render of the SignUpCTA component goes here */}
            <FeaturedImg />
            <Introduction />
+           <Footer />
         </React.Fragment>
     )
 }
