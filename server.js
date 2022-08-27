128e0779de15177d09f88c91942509fab1be7c98
Gary Grant <garygrant693@gmail.com>
2022-08-26 23:32:07 -0400

    made corrections to config.env and mongoose connect

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path:"./config.env"});
 const PORT  = process.env.PORT || 5000;
+const db = process.env.DATABASE
 const app = require('./app');
 
 
 // mongoose connect 
-mongoose.connect("mongodb+srv://imperial-models:078ltUsgrqsbuaDn@imperial-models.nie9vb7.mongodb.net/?retryWrites=true&w=majority"
-).then(()=> console.log("DB connection was successful"));
+mongoose.connect(db)
+.then(()=> console.log('Mongo DB connection was successful'))
 
 app.listen(PORT,()=>{
-    console.log(`Sever is listening on port ${PORT}`)
+    console.log(`Server is listening on port ${PORT}`)
 })
