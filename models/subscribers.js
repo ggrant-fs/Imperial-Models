50d9834383be1ed9a477e88d45c53833c86fbffc
Gary Grant <garygrant693@gmail.com>
2022-09-01 19:58:16 -0400

    created a class constructor to seed that databases

--- models/subscribers.js
+++ models/subscribers.js
@@ -4,16 +4,16 @@ const subscriberSchema = new mongoose.Schema({
     
     firstName:{
         type: String,
-        requied:[true,'Schema validation error, each subscription requires a first name, please check models/subscribers.js']
+       requied:[true,'Schema validation error, each subscription requires a first name, please check models/subscribers.js']
     },
 
     lastName:{
         type: String,
-        required:[true,'Schema validation error, each subscription requires a last name, please check models/subscribers.js']
+       required:[true,'Schema validation error, each subscription requires a last name, please check models/subscribers.js']
     },
     email:{
         type: String,
-        required:[true,'Schema validation error, each subscription requires an email, please check models/subscribers.js']
+       required:[true,'Schema validation error, each subscription requires an email, please check models/subscribers.js']
      }
 },{timestamp:true});
 
