624e5c6c8415e78ed35b34270e89a38c6876b5e8
Gary Grant <garygrant693@gmail.com>
2022-09-03 04:22:15 -0400

    added conver photos to FeaturedImg component, and added z-index to header

--- client/src/Component/UI/FeaturedImg.js
+++ client/src/Component/UI/FeaturedImg.js
@@ -1,18 +1,20 @@
 import { Fragment } from 'react';
 import React from 'react';
+import imageOne from '../../images/featuredimg3.jpg';
+import imageTwo from '../../images/featuredimg4.jpg';
 import styles from './FeaturedImg.module.css';
 
 const FeaturedImg = () =>{
     return (
         <Fragment>
-        <section className={styles['featured-photos']}>
-            <div className={styles['featured-photos--image1']}>
-                image 1
+            <div className={styles['feature-photos']}>
+               <div className={`${styles['feature-photos__container']} ${styles.test1}`}>
+                   <img src={imageOne} alt='first feature img' className={styles['feature-photos__img1']}/>
+               </div>
+               <div className={`${styles['feature-photos__container']} ${styles.test2}`}>
+                  <img src={imageTwo} alt='second feature img' className={styles['feature-photos__img2']}/>
+               </div>
             </div>
-            <div className={styles['featured-photos--image2']}>
-                image 2
-            </div>
-        </section>
         </Fragment>
     )
 };
