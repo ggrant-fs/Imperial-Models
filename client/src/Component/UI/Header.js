a20fe01624e111fb003daf4e1c068ef9e4d7ecad
Gary Grant <garygrant693@gmail.com>
2022-09-05 23:01:13 -0400

    image added to Introduction.js componenet

--- client/src/Component/UI/Header.js
+++ client/src/Component/UI/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from 'react';
 import React from 'react';
+import MenuButton from './MenuButton';
 import styles from './Header.module.css';
 
 const Header = (props) =>{
     return (
         <Fragment>
         <header className={styles['main-header']}>
-
+           <MenuButton className={styles['main-header__menu-btn']}/>
         </header>
         </Fragment>
     )
--- client/src/Component/UI/Introduction.js
+++ client/src/Component/UI/Introduction.js
@@ -1,17 +1,17 @@
 import { Fragment } from 'react';
 import React from 'react';
+import maleModel1 from '../../images/femalemodel12.jpg';
 import styles from './Introduction.module.css';
 
 const Introduction = () =>{
     return (
         <Fragment>
-        <section className={styles.intro}>
-            <div className={styles.intro__img}></div>
-            <article className={styles.intro__decription}>
-                <h2 className={styles.intro__title}>Introduction title goes here</h2>
-                <p></p>
-            </article>
-        </section>
+        <div className={styles.intro}>
+            <div className={styles.intro__container}>
+                <img src={maleModel1} alt=' ' className={styles.intro__img}/>
+            </div>
+            <div className={styles.intro__decription}></div>
+        </div>
         </Fragment>
     )
 };
