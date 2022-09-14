ab9714148d87506ae342000497e5fb5f498e56d4
Gary Grant <garygrant693@gmail.com>
2022-09-14 18:51:28 -0400

    created css file SubscriptionModal.module.css

--- client/src/Component/Modal/SubscriptionModal.js
+++ client/src/Component/Modal/SubscriptionModal.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import React from 'react';
+import style from './SubscriptionModal.module.css';
 
 const SubscriptionModal = (props) =>{
     return (
         <Fragment>
             <div>
-                
+
             </div>
         </Fragment>
     )
