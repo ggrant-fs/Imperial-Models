51f65e11dd3e62da6b3ba128d12cf87a483e6532
Gary Grant <garygrant693@gmail.com>
2022-09-14 18:48:09 -0400

    created new react component "SubscriptionModal.js"

---
+++ client/src/Component/Modal/SubscriptionModal.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import React from 'react';
+
+const SubscriptionModal = (props) =>{
+    return (
+        <Fragment>
+            <div>
+                
+            </div>
+        </Fragment>
+    )
+}
+
+export default SubscriptionModal;
