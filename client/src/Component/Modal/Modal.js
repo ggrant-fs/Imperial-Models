657a6b83b83a4a02dd7ca9424233e4ea9f73c109
Gary Grant <garygrant693@gmail.com>
2022-09-29 17:06:52 -0400

    created react portal

--- client/src/Component/Modal/Modal.js
+++ client/src/Component/Modal/Modal.js
@@ -1,9 +1,16 @@
-import { Fragment } from "react";
+import React, { Fragment } from "react";
+import { ReactDOM } from "react-dom";
+import Backdrop from "./Backdrop";
+import ModalOverlay from "./ModalOverlay"
+
+const portal = document.getElementById('overlays');
 
 const Modal =(props)=>{
+
     return(
         <Fragment>
-
+             {ReactDOM.createPortal(<Backdrop/>, portal)}
+             {ReactDOM.createPortal(<ModalOverlay/>, portal)}
         </Fragment>
     )
 }
