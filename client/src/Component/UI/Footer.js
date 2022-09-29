9ab601d2ff0f2f9b3ace048a64f233cf0d02e958
Gary Grant <garygrant693@gmail.com>
2022-09-29 17:49:52 -0400

    removed main-footer__triangle

--- client/src/Component/UI/Footer.js
+++ client/src/Component/UI/Footer.js
@@ -6,7 +6,6 @@ const Footer = (props) =>{
     return (
         <Fragment>
           <footer className={styles['main-footer']}>
-            <div className={styles['main-footer__triangle']}></div>
           </footer>
         </Fragment>
     )
