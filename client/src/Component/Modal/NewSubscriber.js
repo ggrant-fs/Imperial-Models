4e28f3107c5eb8e097ddc2bdeeef94fb07812535
Gary Grant <garygrant693@gmail.com>
2022-10-05 20:02:01 -0400

    added button and content to form

--- client/src/Component/Modal/NewSubscriber.js
+++ client/src/Component/Modal/NewSubscriber.js
@@ -8,7 +8,10 @@ const NewSubscriber = (props) =>{
           <div className={styles.subscriber}>
             <img src={femaleModel5} alt='female modal' className={styles['modal-image']}/>
             <form className={styles.subscriber__form}>
-               
+              <div className={styles.subscriber__cta}>
+                    <p>Subscribe for a chance to join us at this years New York Fashion Week!</p>
+              </div>
+             <button className={styles.subscriber__btn} >Subscribe</button>
             </form>
           </div>
         </Modal>
