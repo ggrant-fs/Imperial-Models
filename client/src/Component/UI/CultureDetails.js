f87dfc1b7ba01c04af1f5595328c2052de5b3f94
Gary Grant <garygrant693@gmail.com>
2022-10-14 20:57:41 -0400

    added content to CultureDetails.js

--- client/src/Component/UI/CultureDetails.js
+++ client/src/Component/UI/CultureDetails.js
@@ -1,8 +1,6 @@
 import { Fragment } from "react";
 import styles from './CultureDetails.module.css';
-// import femalemodel2 from '../../images/featuredimg2.jpg';
 import femalemodel3 from '../../images/femalemodel3.jpg';
-// import femaleModel9 from '../../images/femalemodel9.jpg';
 
 const CultureDetails = (props) =>{
     return (
@@ -11,6 +9,34 @@ const CultureDetails = (props) =>{
               <img className={styles['culture__main-img']} src={femalemodel3} alt=''/>
            </div>
             <div className={styles['culture__img-container2']}> </div>
+            <div className={styles['culture__milan-details']}>
+                <h1 className={styles['culture__title']}>Milan</h1>
+                <p className={styles['culture__milan-paragraph']}>
+                Eget aliquet nibh praesent tristique magna sit amet. Mauris cursus 
+                mattis molestie a iaculis at. Porta lorem mollis aliquam ut porttitor
+                leo a. Ullamcorper a lacus vestibulum sed arcu. Magna eget est lorem
+                ipsum. Tortor condimentum lacinia quis vel eros donec ac odio tempor.
+                Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget 
+                egestas.Nunc pulvinar sapien et ligula ullamcorper malesuada proin 
+                libero. Semper risus in hendrerit gravida rutrum quisque non tellus. 
+                </p>
+            </div>
+            <div className={styles['culture__paris-details']}>
+               <h1 className={styles['culture__title']}>Paris</h1>
+               <p className={styles['culture__paris-paragraph']}>
+                Eget aliquet nibh praesent tristique magna sit amet. Mauris cursus 
+                mattis molestie a iaculis at. Porta lorem mollis aliquam ut porttitor
+                leo a. Ullamcorper a lacus vestibulum sed arcu. Magna eget est lorem
+                ipsum. Tortor condimentum lacinia quis vel eros donec ac odio tempor.
+                Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget 
+                egestas.
+              </p>
+              <p>
+               Feugiat sed lectus vestibulum mattis ullamcorper velit sed. A erat
+               nam at lectus urna duis. Posuere lorem ipsum dolor sit. Purus in massa
+               tempor nec feugiat nisl pretium. Purus faucibus ornare suspendisse 
+              </p>
+            </div>
             <div className={styles['culture__img-container3']}></div>
         </Fragment>
     )
