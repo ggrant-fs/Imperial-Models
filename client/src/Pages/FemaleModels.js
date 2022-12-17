1a67b459591a76d94211be4cafb3c60c35bf4317
Gary Grant <garygrant693@gmail.com>
2022-12-16 22:26:31 -0500

    changed pixles to rem

--- client/src/Pages/FemaleModels.js
+++ client/src/Pages/FemaleModels.js
@@ -6,7 +6,7 @@ import JoinUs from "../Component/UI/JoinUs";
 import femImg1 from '../images/femalemodel13.jpg';
 import femImg2 from '../images/femalemodel16.jpg';
 import femImg3 from '../images/femalemodel11.jpg';
-import testimg from '../images/femalemodel19.jpg';
+import femImg19 from '../images/femalemodel19.jpg';
 import styles from './FemaleModels.module.css';
 
 const FemaleModels = (props) =>{
@@ -21,7 +21,7 @@ const FemaleModels = (props) =>{
        <Fragment>
          <main>
           <Layout>
-             <Mainimg images={testimg} className={`${styles["test-img"]} ${styles["main-img"]}`}/>
+             <Mainimg images={femImg19} className={`${styles["fem-img"]} ${styles["main-img"]}`}/>
              <OurModels imgs={modelImgs}/>
              <JoinUs/>
           </Layout>
