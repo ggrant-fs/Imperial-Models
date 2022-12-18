98236b5688865f3a557eb4ac42e084ed60c514de
Gary Grant <garygrant693@gmail.com>
2022-12-18 02:27:30 -0500

    chnaged css active feature and color

--- client/src/Component/UI/Header.js
+++ client/src/Component/UI/Header.js
@@ -12,6 +12,14 @@ const Header = (props) =>{
            <i className="gg-menu"></i>
            </div>
         </header>
+        {/* ========================================
+        Then navigation componenet goes here.
+        I'm putting it into it's own component 
+        because of the amount of functionality 
+        that will go into the navigation, it will
+        also reduce the amount of html and jsx in
+        the module
+        ======================================== */}
         </Fragment>
     )
 };
