c9a3edbecba79190902523a632234fe95c28a49d
Gary Grant <garygrant693@gmail.com>
2023-01-09 22:44:16 -0500

    added styling to submit button

--- client/src/Component/UI/Nav.js
+++ client/src/Component/UI/Nav.js
@@ -9,7 +9,7 @@ const Nav = (props) =>{
             <nav className={styles.nav} onClick={props.sideBar}>
                 <ul>
                     <li><NavLink to='/home' activeClassName={styles.active}>Home</NavLink></li>
-                    <li><NavLink to='/contact' activeClassName={styles.active}>About</NavLink></li>
+                    {/* <li><NavLink to='/contact' activeClassName={styles.active}>About</NavLink></li> */}
                     <li><NavLink to='/models-females' activeClassName={styles.active}>Models</NavLink></li>
                     <li ><NavLink to='/models-males' activeClassName={styles.active} >Male Models</NavLink></li>
                     <li><NavLink to='/contact' activeClassName={styles.active}>Contact</NavLink></li>
