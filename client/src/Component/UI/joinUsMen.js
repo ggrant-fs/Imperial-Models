17e922901c92bac3d665442164ce3ccf18295519
Gary Grant <garygrant693@gmail.com>
2023-01-14 22:52:14 -0500

    finished styling JoinUsMen component

--- client/src/Component/UI/joinUsMen.js
+++ client/src/Component/UI/joinUsMen.js
@@ -5,9 +5,13 @@ const JoinUsMen = (props) =>{
     return (
         <Fragment>
             <div className={styles["intro-container"]}>
-              <div className={styles.intro}> </div>
-            </div>
-           
+              <div className={styles.intro}>
+                <div className={styles["intro__description"]}>
+                    <h1 className={styles["intro__heading"]}>Join the number one male model agency in NYC Paris and Milan</h1>
+                </div>
+                <button className={styles['joinUs__button']}>Join us </button>
+              </div>
+            </div>       
         </Fragment>
     )
 }
