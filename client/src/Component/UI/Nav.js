87ca252e4b2b7ba8621c50b06fc122e0266330cd
Gary Grant <garygrant693@gmail.com>
2023-01-20 00:49:18 -0500

    project is fully responsive

--- client/src/Component/UI/Nav.js
+++ client/src/Component/UI/Nav.js
@@ -8,11 +8,11 @@ const Nav = (props) =>{
         <Fragment>
             <nav className={styles.nav} onClick={props.sideBar}>
                 <ul>
-                    <li><NavLink to='/home' activeClassName={styles.active}>Home</NavLink></li>
+                    <li className={styles["nav-item"]}><NavLink to='/home' activeClassName={styles.active}>Home</NavLink></li>
                     {/* <li><NavLink to='/contact' activeClassName={styles.active}>About</NavLink></li> */}
-                    <li><NavLink to='/models-females' activeClassName={styles.active}>Models</NavLink></li>
-                    <li ><NavLink to='/models-males' activeClassName={styles.active} >Male Models</NavLink></li>
-                    <li><NavLink to='/contact' activeClassName={styles.active}>Contact</NavLink></li>
+                    <li className={styles["nav-item"]}><NavLink to='/models-females' activeClassName={styles.active}>Models</NavLink></li>
+                    <li  className={styles["nav-item"]}><NavLink to='/models-males' activeClassName={styles.active} >Male Models</NavLink></li>
+                    <li className={styles["nav-item"]}><NavLink to='/contact' activeClassName={styles.active}>Contact</NavLink></li>
                 </ul>
             </nav>
         </Fragment>
